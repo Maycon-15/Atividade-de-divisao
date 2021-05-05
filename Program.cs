96a67f68ea6e4f11b297dadc2267674e6a023711
Maycon-15 <78987309+Maycon-15@users.noreply.github.com>
2021-05-05 20:36:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Numerador, Denominador, Resultado;
+
+            Console.WriteLine("DIVISÃO");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o numerador da divisão:");
+            Numerador = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o denominador da divisão:");
+            Denominador = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Resultado = Numerador / Denominador;
+
+            if (Denominador == 0){
+            Console.WriteLine("Zero nao pode ser dividido");
+            }
+            else{
+
+            Console.WriteLine($"O resultado é: {Resultado}");
+            }
+                        
+        }
+    }
+}
